a77e48bcb1a355b438145c677b39f681eddb924a
Android <thangapandi275@gmail.com>
2019-09-23 13:31:21 +0530

    starting 1.0 starting

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,12 +1,57 @@
 <?php
+
 class Home extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
     }
+
     public function index()
     {
         $this->load->view('home');
+    }
+
+    public function a()
+    {
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $phone = $_GET['phone'];
+        $password = $_GET['password'];
+        $sid = $_GET['uid'];
+
+        $data = array('name' => $name,
+            'password' => $password,
+            'email' => $email,
+            'phone' => $phone,
+            'uid' => $sid);
+
+        $val = $this->education->store($data);
+        if ($val == 1) {
+            $status = "ok";
+        } else {
+            $status = "failed";
+        }
+
+        echo json_encode(array("response" => $status));
+
+    }
+
+    public function b()
+    {
+        $email = $_GET['email'];
+        $password = $_GET['password'];
+
+        $val = $this->education->logcheck($email);
+        $pass = $val[0]['password'];
+        if ($password == $pass) {
+            $id = $val[0]['id'];
+            $name = $val[0]['name'];
+            $status = "ok";
+            echo json_encode(array('response'=>$status, 'id'=>$id, 'name' => $name));
+        } else {
+            $status = "failed";
+            echo json_encode(array('response'=>$status));
+        }
     }
 }
--- application/models/Education.php
+++ application/models/Education.php
@@ -5,4 +5,16 @@ class Education extends CI_Model
     {
         parent::__construct();
     }
+
+    public function store($data)
+    {
+        $val = $this->db->insert('users', $data);
+        return $val;
+    }
+    public function logcheck($email)
+    {
+        $sql = "select * from users where email='$email' or uid='$email'";
+        $val = $this->db->query($sql);
+        return $val->result_array();
+    }
 }
